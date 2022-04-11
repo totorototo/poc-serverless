04cfb6acd17cc0c3aa225c8def9c27b201887a4f
Thomas Gilbert <thomas.gilbert@dolby.com>
2022-04-11 16:56:27 +0200

    read posted file

--- api/index.js
+++ api/index.js
@@ -1,42 +1,15 @@
-import { transform, prettyPrint } from "camaro";
-
-const xml = `
-    <players>
-        <player jerseyNumber="10">
-            <name>wayne rooney</name>
-            <isRetired>false</isRetired>
-            <yearOfBirth>1985</yearOfBirth>
-        </player>
-        <player jerseyNumber="7">
-            <name>cristiano ronaldo</name>
-            <isRetired>false</isRetired>
-            <yearOfBirth>1985</yearOfBirth>
-        </player>
-        <player jerseyNumber="7">
-            <name>eric cantona</name>
-            <isRetired>true</isRetired>
-            <yearOfBirth>1966</yearOfBirth>
-        </player>
-    </players>
-`;
-
-const template = [
-  "players/player",
-  {
-    name: "title-case(name)",
-    jerseyNumber: "@jerseyNumber",
-    yearOfBirth: "number(yearOfBirth)",
-    isRetired: 'boolean(isRetired = "true")',
-  },
-];
-
-const getJSON = async () => {
-  return await transform(xml, template);
-};
+import * as fs from "fs";
 
 module.exports = async (req, res) => {
   try {
-    res.send(await getJSON());
+    const file = req.query.file;
+    if (file) {
+      fs.readFile(file, "utf8", (err, data) => {
+        if (!err) {
+          res.send(data);
+        }
+      });
+    }
   } catch (err) {
     res.send(err); // send the thrown error
   }
