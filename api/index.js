1c1ca40a341e3eb9fe210d3a875914e7ee4adf2b
Thomas Gilbert <thomas.gilbert@dolby.com>
2022-04-11 16:14:41 +0200

    add camaro and prettier

--- api/index.js
+++ api/index.js
@@ -1,21 +1,47 @@
-const Genius = require("genius-lyrics") // import the library
-const Client = new Genius.Client() // setup the Genius Lyrics API
+import { transform, prettyPrint } from "camaro";
 
-async function retrieveLyrics() {
-    const songs = await Client.songs.search("Pop Smoke") // Get songs from Pop Smoke
+const xml = `
+    <players>
+        <player jerseyNumber="10">
+            <name>wayne rooney</name>
+            <isRetired>false</isRetired>
+            <yearOfBirth>1985</yearOfBirth>
+        </player>
+        <player jerseyNumber="7">
+            <name>cristiano ronaldo</name>
+            <isRetired>false</isRetired>
+            <yearOfBirth>1985</yearOfBirth>
+        </player>
+        <player jerseyNumber="7">
+            <name>eric cantona</name>
+            <isRetired>true</isRetired>
+            <yearOfBirth>1966</yearOfBirth>
+        </player>
+    </players>
+`;
 
-    const indexSongs = Math.floor(Math.random() * Math.floor(songs.length))
-    const lyrics = await songs[indexSongs].lyrics() // Get random song
+const template = [
+  "players/player",
+  {
+    name: "title-case(name)",
+    jerseyNumber: "@jerseyNumber",
+    yearOfBirth: "number(yearOfBirth)",
+    isRetired: 'boolean(isRetired = "true")',
+  },
+];
 
-    const arrLyrics = lyrics.split("\n").filter(l => l.length && l[0] !== '[')
-    const indexLyrics = Math.floor(Math.random() * Math.floor(arrLyrics.length))
-    return arrLyrics[indexLyrics] // Return random lyrics
-}
+const getJSON = async () => {
+  const result = await transform(xml, template);
+  console.log(result);
 
-module.exports = async (req, res) => { // this function will be launched when the API is called.
-    try {
-        res.send(await retrieveLyrics()) // send the lyrics
-    } catch (err) {
-        res.send(err) // send the thrown error
-    }
-}
+  const prettyStr = await prettyPrint(xml, { indentSize: 4 });
+  console.log(prettyStr);
+};
+
+module.exports = async (req, res) => {
+  try {
+    res.send(await getJSON());
+  } catch (err) {
+    res.send(err); // send the thrown error
+  }
+};
